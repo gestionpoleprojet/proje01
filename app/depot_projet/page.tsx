88038fef2600323ccce10a0a3a168077b8f1890c
Dreamsvilleteam <marc.pierre.jean.sahuguet@ut.ee>
2023-02-17 12:59:51 +0100

    last push

--- app/depot_projet/page.tsx
+++ app/depot_projet/page.tsx
@@ -59,8 +59,35 @@ export default function Depot_Projet() {
               <a href="https://vpe-ece.com" className="getstarted-bttn text-2xl leading-8 w-button" style={{margin: "auto"}}
                 ><strong className="bold-text">Déposer mon projet</strong></a>
             </div>
+
+            <img
+                src="./images/assos.webp"
+                width="540"
+              id="w-node-_89b30bd8-a8db-67a3-614b-b071ede6a2cf-d135d194"
+              alt="couple-working-on-laptop"
+              className="image-7"
+              
+            />
+            <div
+              id="w-node-_138c1701-d088-8486-1d67-68f5d6bd7ca0-d135d194"
+              className="hero-wrapper"
+            >
+              <h1 className="hero-title mb-2 hero-mobile">
+                Je suis une association
+              </h1>
+              <div className="hero-text text-base leading-4">
+                Depuis 2012, le programme Valorisation des Projets Etudiants
+                (VPE) de l’ECE permet aux élèves ingénieurs de collaborer et
+                d&#x27;accompagner.
+              </div>
+              <a href="https://vpe-ece.com" className="getstarted-bttn text-2xl leading-8 w-button" style={{margin: "auto"}}
+                ><strong className="bold-text">Déposer mon projet</strong></a>
+            </div>
+            
             
           </div>
+
+          
 
         </div>
       </div>
